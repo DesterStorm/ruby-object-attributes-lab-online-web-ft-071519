ad28dce75187a7128cb7029fb70014f713ae6967
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-27 19:48:39 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,30 +1,19 @@
 class Person
   
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
+  def name=(name)
+    @name = name
   end
   
-  def name=(full_name)
-    first_name, last_name = full_name.split
-    @first_name = first_name
-    @last_name = last_name
+  def name
+    @name
   end
+
+  def job=(job)
+    @job = job
+  end 
+
+  def job 
+    @job
+  end 
   
-  def name 
-    "#{@first_name} #{@last_name}".strip
-  end
-  
-  def initialize(job)
-    @singer = singer 
-  end
-  
-  def job=(singer)
-    singer = job
-    @singer = singer
-  end
-  
-  def job
-    "#{@singer}"
-  end
 end
